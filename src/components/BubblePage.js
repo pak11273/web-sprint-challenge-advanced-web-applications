2c1668a2af97e1e756b4a70e3194d5b69704f714
pak11273 <pak11273@gmail.com>
2021-02-26 10:38:23 -0600

    fetches bubble data

--- src/components/BubblePage.js
+++ src/components/BubblePage.js
@@ -3,9 +3,19 @@ import React, { useEffect, useState } from "react";
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 import axios from "axios";
+import { axiosWithAuth } from "../helpers/axiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+  useEffect(() => {
+    console.log(axiosWithAuth);
+    axiosWithAuth()
+      .get("/api/colors")
+      .then((res) => {
+        setColorList(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <>
--- src/helpers/axiosWithAuth.js
+++ src/helpers/axiosWithAuth.js
@@ -5,15 +5,16 @@ import axios from "axios";
 //Build and export a function used to send in our authorization token
 
 export const axiosWithAuth = () => {
-  const token = window.localStorage.getItem("token");
-  if (token) {
-    return axios.create({
-      headers: {
-        authorization: token,
-      },
-      baseURL: "http://localhost:5000",
-    });
-  } else {
-    throw new Error("you must have a token to do this");
-  }
+  const token = JSON.parse(localStorage.getItem("token"));
+  // if (token) {
+
+  return axios.create({
+    headers: {
+      authorization: token,
+    },
+    baseURL: "http://localhost:5000",
+  });
+  // } else {
+  //   throw new Error("you must have a token to do this");
+  // }
 };
